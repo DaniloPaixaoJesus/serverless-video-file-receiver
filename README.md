e66512edbf6a64e483bbabf00ec5aeb2ec1dd7df
DaniloPaixaoJesus <danilo.oficial@gmail.com>
2024-05-28 20:25:25 -0300

    aws

--- README.md
+++ README.md
@@ -1,3 +1,41 @@
-# serverless-video-file-receiver
 
-aws --endpoint-url=http://localhost:4566 s3 cp teste.mkv s3://transcription-bucket/video-download-from-front-end/teste.mkv
+event test
+
+{
+  "Records": [
+    {
+      "eventVersion": "2.1",
+      "eventSource": "aws:s3",
+      "awsRegion": "us-east-1",
+      "eventTime": "2024-05-25T22:04:17.720Z",
+      "eventName": "ObjectCreated:Put",
+      "userIdentity": {
+        "principalId": "A3RRF1OJZHFQGA"
+      },
+      "requestParameters": {
+        "sourceIPAddress": "187.107.8.125"
+      },
+      "responseElements": {
+        "x-amz-request-id": "XPCSWDPRY5NFRTBC",
+        "x-amz-id-2": "FTxjFXvUZxBgA1kA0dQp2CkV4UZmecVg8wCCTOACYxN9e5SsENo6lKKXvFFfBkDgXEL7oIZm0Y480raE1rMP+Z/uy7NA6SiS"
+      },
+      "s3": {
+        "s3SchemaVersion": "1.0",
+        "configurationId": "3f694aa9-99a4-4684-9582-f50a81ca1a60",
+        "bucket": {
+          "name": "app-transcription-bucket",
+          "ownerIdentity": {
+            "principalId": "A3RRF1OJZHFQGA"
+          },
+          "arn": "arn:aws:s3:::app-transcription-bucket"
+        },
+        "object": {
+          "key": "video-download-from-front-end2/teste1-11.mkv",
+          "size": 536932,
+          "eTag": "dfa07282ced2e7f114f926d126b73509",
+          "sequencer": "0066526061A07E5426"
+        }
+      }
+    }
+  ]
+}
--- lambda_function.py
+++ lambda_function.py
@@ -1,62 +1,32 @@
 import json
 import os
-import boto3
+import uuid
 from urllib.parse import unquote_plus
-
-s3_client = boto3.client('s3', endpoint_url='http://localstack:4566')
-sns_client = boto3.client('sns', endpoint_url='http://localstack:4566')
-topic_arn = os.environ['SNS_TOPIC_ARN']
 
 def lambda_handler(event, context):
     for record in event['Records']:
         bucket_name = record['s3']['bucket']['name']
-        object_key = unquote_plus(record['s3']['object']['key'])
+        bucket_key = unquote_plus(record['s3']['object']['key'])
         object_size = record['s3']['object']['size']
         event_time = record['eventTime']
-        
-        # Generate UUID
-        new_uuid = str(uuid.uuid4())
-        
-        # Extract file extension and base name
-        ext = os.path.splitext(object_key)[1]
-        base_name = os.path.splitext(object_key)[0]
-        
-        # Create new object key
-        new_object_key = f"{base_name}_{new_uuid}{ext}"
-        
-        # Rename the object in S3
-        copy_source = {'Bucket': bucket_name, 'Key': object_key}
-        try:
-            s3_client.copy_object(CopySource=copy_source, Bucket=bucket_name, Key=new_object_key)
-            s3_client.delete_object(Bucket=bucket_name, Key=object_key)
-        except Exception as e:
-            print(f"Erro ao renomear o objeto no S3: {e}")
-            raise e
-        
+
+        # Extract the file name from the bucket key
+        file_name = os.path.basename(bucket_key)
+        print("Receiving file from front-end:")
+        print(f"file_name - {file_name}")
+        print(f"bucket_name - {bucket_name}")
+        print(f"bucket_key - {bucket_key}")
+
         # Create message to send to SNS
         message = {
             "bucket-name": bucket_name,
-            "bucket-key": new_object_key,
+            "bucket-key": bucket_key,
+            "file-name": file_name,
             "size": object_size,
             "event-time": event_time,
-            "uuid": new_uuid
+            "transaction-id": str(uuid.uuid4())
         }
         
-        message_json = json.dumps(message)
-        
-        # Publish the message to SNS
-        try:
-            response = sns_client.publish(
-                TopicArn=topic_arn,
-                Message=message_json
-            )
-        except Exception as e:
-            print(f"Erro ao publicar mensagem no SNS: {e}")
-            raise e
-        
-        print(f"Mensagem publicada com sucesso no SNS: {message}")
+        #message_json = json.dumps(message)
 
-    return {
-        'statusCode': 200,
-        'body': json.dumps('Processamento concluído com sucesso.')
-    }
+    return message
